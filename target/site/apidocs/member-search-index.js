f62eeb0d7b6d9cc9a907a213cf5828c172c50cef
Raferat <mailnaneduveryhodnestranky@seznam.cz>
2021-06-06 14:00:35 +0200

    Made javadoc, added shade plugin to maven, made more efficient way to draw.

--- target/site/apidocs/member-search-index.js
+++
@@ -1,1 +0,0 @@
-memberSearchIndex = [{"p":"cz.stv.neurondemofx","c":"App","l":"App()","url":"%3Cinit%3E()"},{"p":"cz.stv.neurondemofx","c":"App","l":"main(String[])","url":"main(java.lang.String[])"},{"p":"cz.stv.neurondemofx","c":"App","l":"start(Stage)","url":"start(javafx.stage.Stage)"}]
