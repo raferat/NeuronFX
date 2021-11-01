256d85efdd18d60bb8ccbb795e2d44dfe0c5c3d6
martin.vagner <martin.vagner@UCEB07.stv.loc>
2021-11-01 19:02:00 +0100

    Added TrainingImage

--- MnistFileParser/src/cz/martin/mnistfileparser/MnistFileParser.java
+++ MnistFileParser/src/cz/martin/mnistfileparser/MnistFileParser.java
@@ -2,29 +2,41 @@ package cz.martin.mnistfileparser;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.Font;
 import java.awt.Graphics2D;
+import java.awt.GridLayout;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.LinkedList;
 import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class MnistFileParser
 {
 
+  private static int position = 0;
+  
+  private static final LinkedList<TrainingImage> trainingImages = new LinkedList<> ();
+
   private final static String mnistDirectory = "R:\\P04-2021-22\\MNIST";
 
-  private static JLabel icon = new JLabel ();
-  private static JLabel label = new JLabel ();
-  
+  private static final JLabel icon = new JLabel ();
+  private static final JLabel label = new JLabel ();
+
   private static JFrame frame;
 
   private static Image readNextImage ( FileInputStream fis , int size ) throws IOException
@@ -47,18 +59,13 @@ public class MnistFileParser
     return img;
   }
 
-  private static String readNextLabel ( FileInputStream fis ) throws IOException
+  private static int readNextLabel ( FileInputStream fis ) throws IOException
   {
-    return "" + fis.read ();
+    return fis.read ();
   }
 
   private static int fromByteArray ( byte[] bytes )
   {
-    int result = 0;
-    for (int i = 0; i < bytes.length; i++)
-    {
-      result += bytes[i] * Math.pow ( 256 , bytes.length - i - 1 );
-    }
     return bytes[0] << 24 | (bytes[1] & 0xFF) << 16 | (bytes[2] & 0xFF) << 8 | (bytes[3] & 0xFF);
   }
 
@@ -74,19 +81,42 @@ public class MnistFileParser
           0 , 0 , 8 , 3
         } ))
         {
-          stream.readNBytes ( 8 );
-          icon = new JLabel ( new ImageIcon ( readNextImage ( stream , fromByteArray ( stream.readNBytes ( 4 ) ) ) ) );
-          frame.add ( icon );
-          
+          int max = 20;
+          fromByteArray ( stream.readNBytes ( 4 ) );
+          stream.readNBytes ( 4 );
+          int size = fromByteArray ( stream.readNBytes ( 4 ) );
+          if (trainingImages.isEmpty ())
+          {
+            for (int i = 0; i < max; i++)
+            {
+              trainingImages.add ( new TrainingImage (readNextImage ( stream , size )) );
+            }
+          }
+          else
+          {
+            for ( TrainingImage t : trainingImages )
+              t.setImage ( readNextImage ( stream , size ));
+          }
         }
         else if (Arrays.equals ( magicNumber , new byte[]
         {
           0 , 0 , 8 , 1
         } ))
         {
-          stream.readNBytes ( 4 );
-          label . setText ( readNextLabel ( stream));
-          frame . add ( label );
+          int max = 20;
+          fromByteArray ( stream.readNBytes ( 4 ) );
+          if (trainingImages.isEmpty ())
+          {
+            for (int i = 0; i < max; i++)
+            {
+              trainingImages.add ( new TrainingImage (readNextLabel ( stream )) );
+            }
+          }
+          else
+          {
+            for ( TrainingImage t : trainingImages )
+              t.setLabel ( readNextLabel ( stream ));
+          }
         }
 
       }
@@ -96,29 +126,64 @@ public class MnistFileParser
       }
 
     }
-    frame.setVisible ( true );
   }
 
   public static void main ( String[] args )
   {
+    label.setFont ( new Font (Font.SERIF, Font.BOLD, 40));
     frame = new JFrame ( "Select input files" );
-    frame . setLayout ( new BoxLayout (frame.getContentPane (), BoxLayout.Y_AXIS));
     frame.setDefaultCloseOperation ( JFrame.EXIT_ON_CLOSE );
     var d = new javax.swing.JFileChooser ( mnistDirectory );
-    d.setFileSelectionMode ( JFileChooser.FILES_ONLY);
-    d . setMultiSelectionEnabled(true);
+    d.setFileSelectionMode ( JFileChooser.FILES_ONLY );
+    d.setMultiSelectionEnabled ( true );
     frame.add ( d );
 
     frame.setSize ( 900 , 500 );
     frame.setVisible ( true );
 
-    d.addActionListener ( (ActionEvent e) ->
+    d.addActionListener ((ActionEvent e) ->
     {
       frame.setVisible ( false );
       frame.remove ( d );
       parse ( d.getSelectedFiles () );
+      updatePosition ();
+      GridLayout layout = new GridLayout (2, 1);
+      JPanel j = new JPanel (layout);
+      j . add ( icon );
+      j . add ( label );
+      frame . add ( j);
+      frame . setVisible ( true );
     } );
+    
+    frame.addKeyListener ( new KeyAdapter ()
+    {
+      @Override
+      public void keyPressed ( KeyEvent e )
+      {
+        if ( e . getKeyCode () == KeyEvent.VK_PAGE_DOWN)
+        {
+          position = ( position + 1 ) % trainingImages.size ();
+          updatePosition ();
+        }
+        else if ( e . getKeyCode () == KeyEvent.VK_PAGE_UP)
+        {
+          position = (( position - 1 ) < 0 ? trainingImages.size () - 1 : position - 1);
+          updatePosition ();
+        }
+      }
+      
+    });
+    
 
+  }
+
+  private static void updatePosition ()
+  {
+    frame . setMinimumSize ( new Dimension (trainingImages.get ( 0 ) . getImage ().getWidth ( frame.getRootPane () ) , trainingImages.get ( 0 ) . getImage ().getHeight ( frame.getRootPane () ) * 2 ));
+    frame . setSize ( new Dimension (trainingImages.get ( 0 ) . getImage ().getWidth ( frame.getRootPane () ) , trainingImages.get ( 0 ) . getImage ().getHeight ( frame.getRootPane () ) * 2 ));
+    frame . setResizable ( false );
+    icon.setIcon ( new ImageIcon (trainingImages.get(position).getImage ()));
+    label.setText ( trainingImages.get(position).getLabel () + "");
   }
 
 }
---
+++ MnistFileParser/src/cz/martin/mnistfileparser/TrainingImage.java
@@ -0,0 +1,40 @@
+package cz.martin.mnistfileparser;
+
+import java.awt.Image;
+
+public class TrainingImage
+{
+  private Image image;
+  private int label;
+
+  public TrainingImage ( Image img )
+  {
+    image = img;
+  }
+
+  public TrainingImage ( int label )
+  {
+    this . label = label;
+  }
+  
+  public Image getImage ()
+  {
+    return image;
+  }
+
+  public int getLabel ()
+  {
+    return label;
+  }
+
+  public void setImage ( Image image )
+  {
+    this.image = image;
+  }
+
+  public void setLabel ( int label )
+  {
+    this.label = label;
+  }
+  
+}
